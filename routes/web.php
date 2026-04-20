d109b93127f06246a1095fb9306765c0fef15c75
MarioSyaif <mariosyaif@gmail.com>
2026-04-20 14:25:17 +0700

    feat: Perbaikan manajemen pasien dan penambahan modul obat

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\PoliController;
+use App\Http\Controllers\Admin\PoliController as AdminPoliController;
 use App\Http\Controllers\Admin\DokterController;
 use App\Http\Controllers\Admin\PasienController;
 use App\Http\Controllers\Admin\ObatController;
+use App\Http\Controllers\Dokter\JadwalPeriksaController;
+use App\Http\Controllers\Pasien\PoliController as PasienPoliController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -23,7 +25,7 @@ Route::middleware(['auth', 'role:admin'])->prefix('admin')->group(function () {
         return view('admin.dashboard');
     })->name('admin.dashboard');
     
-    Route::resource('polis', PoliController::class);        
+    Route::resource('polis', AdminPoliController::class);        
     Route::resource('dokter', DokterController::class);
     Route::resource('pasien', PasienController::class);
     Route::resource('obat', ObatController::class);     
@@ -34,6 +36,7 @@ Route::middleware(['auth', 'role:dokter'])->prefix('dokter')->group(function () 
     Route::get('/dashboard', function () {
         return view('dokter.dashboard');
     })->name('dokter.dashboard');
+     Route::resource('jadwal-periksa', JadwalPeriksaController::class);
 });
 
 // Pasien Routes
@@ -41,4 +44,9 @@ Route::middleware(['auth', 'role:pasien'])->prefix('pasien')->group(function () 
     Route::get('/dashboard', function () {
         return view('pasien.dashboard');
     })->name('pasien.dashboard');
+    
+    // GANTI PasienPoliController jadi PoliController
+    Route::get('/daftar', [PasienPoliController::class, 'get'])->name('pasien.daftar');
+    Route::post('/daftar', [PasienPoliController::class, 'submit'])->name('pasien.daftar.submit');
 });
+
